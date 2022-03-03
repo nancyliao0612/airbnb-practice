ac4794dc0b8a973b2f22c07bcb82a424acbd846c
Nancy <nancy62924@gmail.com>
2022-03-03 21:57:47 +0800

    practice

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,29 @@
 import star from "../images/star-icon.png";
 
 function Card(props) {
-  // console.log(props);
+  const { openSpots, coverImg, stats, location, price, title } = props;
+
   let badgeText;
-  if (props.openSpots === 0) {
+  if (openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.location === "Online") {
+  } else if (location === "Online") {
     badgeText = "ONLINE";
   }
 
   return (
     <div className="card">
-      <img src={props.coverImg} alt="image" className="card-photo" />
+      <img src={coverImg} alt="image" className="card-photo" />
       {badgeText && <button>{badgeText}</button>}
       <div className="text-container">
         <img src={star} alt="star-icon" className="star-icon" />
-        <span>{props.stats.rating} </span>
+        <span>{stats.rating} </span>
         <span className="gray-text">
-          ({props.stats.reviewCount}) • {props.location}
+          ({stats.reviewCount}) • {location}
         </span>
       </div>
-      <p>{props.title}</p>
+      <p>{title}</p>
       <p>
-        <b>From ${props.price}</b> / person
+        <b>From ${price}</b> / person
       </p>
     </div>
   );
